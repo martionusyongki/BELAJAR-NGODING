b75478870be5eba580e523e994c3451ad85bd59f
martionusyongki <martionusyongki@gmail.com>
2024-10-21 14:32:02 +0700

    NEW

---
+++ JAVA/belajar-spring-dasar/src/main/java/lcy/spring/core/BeanConfiguration.java
@@ -0,0 +1,17 @@
+package lcy.spring.core;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import lcy.spring.core.data.Foo;
+
+@Slf4j
+@Configuration
+public class BeanConfiguration {
+    @Bean
+    public Foo foo() {
+        Foo foo = new Foo();
+        log.info("Create new foo");
+        return foo;
+    }
+}
---
+++ JAVA/belajar-spring-dasar/src/main/java/lcy/spring/core/data/Foo.java
@@ -0,0 +1,4 @@
+package lcy.spring.core.data;
+
+public class Foo {
+}
