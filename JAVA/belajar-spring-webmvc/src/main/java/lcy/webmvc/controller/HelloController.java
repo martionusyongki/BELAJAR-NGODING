2df35135aaab6ccd93e13d115538b5408fd08698
martionusyongki <martionusyongki@gmail.com>
2024-10-22 21:54:11 +0700

    Date Converter

--- JAVA/belajar-spring-webmvc/src/main/java/lcy/webmvc/controller/HelloController.java
+++ JAVA/belajar-spring-webmvc/src/main/java/lcy/webmvc/controller/HelloController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -18,16 +19,9 @@ public class HelloController {
     private HelloService helloService;
 
     @GetMapping(path = "/hello")
-    public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String name = request.getParameter("name");
+    public void helloWorld(@RequestParam(name = "name", required = false) String name,
+                           HttpServletResponse response) throws IOException {
         String responseBody = helloService.hello(name);
-
-//        if (Objects.isNull(name)) {
-//            name = "Guest";
-//        }
-
-//        response.getWriter().println("Hello " + name);
-
         response.getWriter().println(responseBody);
     }
 }
