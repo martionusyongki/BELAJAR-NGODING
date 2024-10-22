d31c613770246196dbdc291361f8e07328728e19
martionusyongki <martionusyongki@gmail.com>
2024-10-22 21:19:41 +0700

    Mock Bean

--- JAVA/belajar-spring-webmvc/src/main/java/lcy/webmvc/controller/HelloController.java
+++ JAVA/belajar-spring-webmvc/src/main/java/lcy/webmvc/controller/HelloController.java
@@ -2,6 +2,8 @@ package lcy.webmvc.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lcy.webmvc.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,14 +12,20 @@ import java.util.Objects;
 
 @Controller
 public class HelloController {
+    @Autowired
+    private HelloService helloService;
 
     @RequestMapping(path = "/hello")
     public void helloWorld(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String name = request.getParameter("name");
-        if (Objects.isNull(name)) {
-            name = "Guest";
-        }
+        String responseBody = helloService.hello(name);
 
-        response.getWriter().println("Hello " + name);
+//        if (Objects.isNull(name)) {
+//            name = "Guest";
+//        }
+
+//        response.getWriter().println("Hello " + name);
+
+        response.getWriter().println(responseBody);
     }
 }
